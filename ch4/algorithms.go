4454adddf64914fed487ab764aa9b7eed8f49c5d
Jeremy Stein <stei0805@umn.edu>
2022-03-21 16:58:33 -0400

    Finished 4.1

--- ch4/algorithms.go
+++ ch4/algorithms.go
@@ -1,6 +1,6 @@
 package ch2
 
-// Brute Force Maximum subarray - 4.1 in-chapter algo
+// Brute Force Maximum subarray - 4.1 in-chapter algo & Ex 4.1-2
 //
 // This is the first solution proposed for the volatile chemical
 // company stock price problem before transforming the input to allow
@@ -15,9 +15,22 @@ package ch2
 //
 // So we buy at 0, hold until 10, and then sell.
 func BruteMaximumSubarray(arr []int) []int {
-	if len(arr) == 0 {
+	if len(arr) == 0 || len(arr) == 1 {
 		return arr
 	}
+
+	valid := false
+	prev := arr[0]
+	for i := 1; i < len(arr); i++ {
+		if arr[i]-prev >= 0 {
+			valid = true
+		}
+		prev = arr[i]
+	}
+	if !valid {
+		return []int{}
+	}
+
 	globalMaxIdx := 0
 	globalMinIdx := 0
 	localMaxIdx := 0
@@ -34,4 +47,224 @@ func BruteMaximumSubarray(arr []int) []int {
 		}
 	}
 	return arr[globalMinIdx : globalMaxIdx+1]
+}
+
+// Divide-and-conquer Maximum Subarray - 4.1 in-chapter algorithm
+//
+// This algorithm approaches the previous problem, the volatile company
+// stock price problem, using divide-and-conquer to allow us to get a better
+// running time: o(n^2). Note the difference between the two: the first had a
+// lower bound of n^2, this one should have a loose upper bound of n^2 and
+// should therefore be strictly faster.
+//
+// Note: You can assume that there is one and only one maximum subarray in
+// each test case.
+//
+// Interface is identical to brute force algorithm.
+func MaximumSubarray(arr []int) []int {
+	if len(arr) == 0 || len(arr) == 1 {
+		return arr
+	}
+
+	valid := false
+	prev := arr[0]
+	for i := 1; i < len(arr); i++ {
+		if arr[i]-prev >= 0 {
+			valid = true
+		}
+		prev = arr[i]
+	}
+	if !valid {
+		return []int{}
+	}
+
+	daily_changes := make([]int, 0)
+	for i := 1; i < len(arr); i++ {
+		daily_changes = append(daily_changes, arr[i]-arr[i-1])
+	}
+
+	result := findMaxSubarray(daily_changes, 0, len(daily_changes)-1)
+	low := result[0]
+	high := result[1]
+	return arr[low : high+2]
+}
+
+func findMaxSubarray(arr []int, low int, high int) [3]int {
+	if low == high {
+		return [3]int{low, high, arr[low]}
+	} else {
+		mid := int((low + high) / 2)
+
+		leftResult := findMaxSubarray(arr, low, mid)
+		leftLow := leftResult[0]
+		leftHigh := leftResult[1]
+		leftSum := leftResult[2]
+
+		rightResult := findMaxSubarray(arr, mid+1, high)
+		rightLow := rightResult[0]
+		rightHigh := rightResult[1]
+		rightSum := rightResult[2]
+
+		crossResult := findMaxCrossingSubarray(arr, low, mid, high)
+		crossLow := crossResult[0]
+		crossHigh := crossResult[1]
+		crossSum := crossResult[2]
+
+		if leftSum >= rightSum && leftSum >= crossSum {
+			return [3]int{leftLow, leftHigh, leftSum}
+		} else if rightSum >= leftSum && rightSum >= crossSum {
+			return [3]int{rightLow, rightHigh, rightSum}
+		} else {
+			return [3]int{crossLow, crossHigh, crossSum}
+		}
+	}
+}
+
+func findMaxCrossingSubarray(arr []int, low int, mid int, high int) [3]int {
+	leftSum := -100000
+	maxLeft := 0
+	maxRight := 0
+	sum := 0
+	for i := mid; i >= low; i-- {
+		sum += arr[i]
+		if sum > leftSum {
+			leftSum = sum
+			maxLeft = i
+		}
+	}
+
+	rightSum := -100000
+	sum = 0
+
+	for i := mid + 1; i <= high; i++ {
+		sum += arr[i]
+		if sum > rightSum {
+			rightSum = sum
+			maxRight = i
+		}
+	}
+	result := [3]int{maxLeft, maxRight, leftSum + rightSum}
+	return result
+}
+
+// 4.1-3 Coarsening the max subarray algorithm
+//
+// Use a brute force implementation to coarsen the leaves of the
+// divide-and-conquer approach and speed up the algorithm.
+//
+// Interface is identical to previous divide and conquer algorithm.
+func CoarsenedMaximumSubarray(arr []int) []int {
+	if len(arr) == 0 || len(arr) == 1 {
+		return arr
+	}
+
+	valid := false
+	prev := arr[0]
+	for i := 1; i < len(arr); i++ {
+		if arr[i]-prev >= 0 {
+			valid = true
+		}
+		prev = arr[i]
+	}
+	if !valid {
+		return []int{}
+	}
+
+	daily_changes := make([]int, 0)
+	for i := 1; i < len(arr); i++ {
+		daily_changes = append(daily_changes, arr[i]-arr[i-1])
+	}
+
+	result := coarseMaxSubarray(daily_changes, 0, len(daily_changes)-1)
+	low := result[0]
+	high := result[1]
+	return arr[low : high+2]
+}
+
+func coarseMaxSubarray(arr []int, low int, high int) [3]int {
+	if low == high {
+		return [3]int{low, high, arr[low]}
+	} else if high-low < 2 {
+		array := BruteMaximumSubarray(arr[low:high])
+		if len(array) == 2 {
+			return [3]int{low, high, arr[low] + arr[high]}
+		} else if array[0] == arr[low] {
+			return [3]int{low, low, arr[low]}
+		} else {
+			return [3]int{high, high, arr[high]}
+		}
+	} else {
+		mid := int((low + high) / 2)
+
+		leftResult := findMaxSubarray(arr, low, mid)
+		leftLow := leftResult[0]
+		leftHigh := leftResult[1]
+		leftSum := leftResult[2]
+
+		rightResult := findMaxSubarray(arr, mid+1, high)
+		rightLow := rightResult[0]
+		rightHigh := rightResult[1]
+		rightSum := rightResult[2]
+
+		crossResult := findMaxCrossingSubarray(arr, low, mid, high)
+		crossLow := crossResult[0]
+		crossHigh := crossResult[1]
+		crossSum := crossResult[2]
+
+		if leftSum >= rightSum && leftSum >= crossSum {
+			return [3]int{leftLow, leftHigh, leftSum}
+		} else if rightSum >= leftSum && rightSum >= crossSum {
+			return [3]int{rightLow, rightHigh, rightSum}
+		} else {
+			return [3]int{crossLow, crossHigh, crossSum}
+		}
+	}
+}
+
+// Ex 4.1-5 Linear time maximum subarrays
+//
+// Interface is identical to previous maximum subarray algorithms.
+func LinearTimeMaximumSubarray(arr []int) []int {
+	if len(arr) == 0 || len(arr) == 1 {
+		return arr
+	}
+
+	valid := false
+	prev := arr[0]
+	for i := 1; i < len(arr); i++ {
+		if arr[i]-prev >= 0 {
+			valid = true
+		}
+		prev = arr[i]
+	}
+	if !valid {
+		return []int{}
+	}
+
+	daily_changes := make([]int, 0)
+	for i := 1; i < len(arr); i++ {
+		daily_changes = append(daily_changes, arr[i]-arr[i-1])
+	}
+
+	maxSum := 0
+	maxStart := 0
+	maxEnd := 0
+	currSum := 0
+	currStart := 0
+
+	for i := 0; i < len(daily_changes); i++ {
+		currSum += daily_changes[i]
+		if currSum > maxSum {
+			maxSum = currSum
+			maxEnd = i
+			maxStart = currStart
+		}
+
+		if currSum < 0 {
+			currSum = 0
+			currStart = i + 1
+		}
+	}
+
+	return arr[maxStart : maxEnd+2]
 }
--- ch4/algorithms_test.go
+++ ch4/algorithms_test.go
@@ -15,14 +15,53 @@ func TestBruteMaximumSubarray(t *testing.T) {
 	}
 }
 
-// func TestMaximumSubarray(t *testing.T) {
-// 	for _, test := range maxSubarrayTests {
-// 		result := MaximumSubarray(test.arr)
-// 		if !areSlicesEqual(result, test.result) {
-// 			t.Errorf("Expected: %v; Got: %v", test.result, result)
-// 		}
-// 	}
-// }
+func TestMaximumSubarray(t *testing.T) {
+	for _, test := range maxSubarrayTests {
+		result := MaximumSubarray(test.arr)
+		if !areSlicesEqual(result, test.result) {
+			t.Errorf("Expected: %v; Got: %v", test.result, result)
+		}
+	}
+}
+
+func TestCoarsenedMaximumSubarray(t *testing.T) {
+	for _, test := range maxSubarrayTests {
+		result := CoarsenedMaximumSubarray(test.arr)
+		if !areSlicesEqual(result, test.result) {
+			t.Errorf("Expected: %v; Got: %v", test.result, result)
+		}
+	}
+}
+
+func TestLinearTimeMaximumSubarray(t *testing.T) {
+	for _, test := range maxSubarrayTests {
+		result := LinearTimeMaximumSubarray(test.arr)
+		if !areSlicesEqual(result, test.result) {
+			t.Errorf("Expected: %v; Got: %v", test.result, result)
+		}
+	}
+}
+
+func TestEmptySubarrayMaximumSubarrays(t *testing.T) {
+	result := BruteMaximumSubarray([]int{3, 2, 1})
+	if !areSlicesEqual(result, []int{}) {
+		t.Errorf("BruteMaximumSubarray - Expected: %v; Got: %v", []int{}, result)
+	}
+	result = MaximumSubarray([]int{3, 2, 1})
+	if !areSlicesEqual(result, []int{}) {
+		t.Errorf("MaximumSubarray - Expected: %v; Got: %v", []int{}, result)
+	}
+	result = CoarsenedMaximumSubarray([]int{3, 2, 1})
+	if !areSlicesEqual(result, []int{}) {
+		t.Errorf("CoarsenedMaximumSubarray - Expected: %v; Got: %v", []int{}, result)
+	}
+	result = LinearTimeMaximumSubarray([]int{3, 2, 1})
+	if !areSlicesEqual(result, []int{}) {
+		t.Errorf("LinearTimeMaximumSubarray - Expected: %v; Got: %v", []int{}, result)
+	}
+}
+
+// * Helpers
 
 type maxSubarray struct {
 	arr    []int
@@ -31,11 +70,13 @@ type maxSubarray struct {
 
 var maxSubarrayTests []maxSubarray = []maxSubarray{
 	{[]int{}, []int{}},
+	{[]int{1}, []int{1}},
 	{[]int{0, 1, 0}, []int{0, 1}},
 	{[]int{1, 2, 0, 3}, []int{0, 3}},
 	{[]int{5, 2, 1, 3, 5}, []int{1, 3, 5}},
 	{[]int{1, 2, 5, 2, 4}, []int{1, 2, 5}},
 	{[]int{1, 2, 5, 0, 10}, []int{0, 10}},
+	{[]int{1, 2, 5, 7, 10}, []int{1, 2, 5, 7, 10}},
 }
 
 func areSlicesEqual(a []int, b []int) bool {
